d6c73b0ed1fc486f1e4fca68fe7537d342538ab7
Avi-Sayen <melinovaalex@gmail.com>
2020-10-22 21:27:05 +0530

    upgdrade to 15

--- js/script.js
+++ js/script.js
@@ -1,10 +1,94 @@
+/* global monogatari */
+
+// Define the messages used in the game.
+monogatari.action ('message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('notification').notifications ({
+	'Welcome': {
+		title: 'Welcome',
+		body: 'This is the Monogatari VN Engine',
+		icon: ''
+	}
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('particles').particles ({
+
+});
+
+// Define the canvas objects used in the game
+monogatari.action ('canvas').objects ({
+
+});
+
+// Credits of the people involved in the creation of this awesome game
+monogatari.configuration ('credits', {
+
+});
+
+
+// Define the images that will be available on your game's image gallery
+monogatari.assets ('gallery', {
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voices', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sounds', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('videos', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+
+});
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+
+});
+
+
+// Define the Characters
+monogatari.characters ({
+	'y': {
+		name: 'Yui',
+		color: '#5bcaff'
+	}
+});
+
 monogatari.script ({
 	// The game starts here.
 	'Start': [
 		'show scene #f7f6f6 with fadeIn',
+		'show notification Welcome',
 		{
 			'Input': {
-				'Text': 'Hello, please enter name?',
+				'Text': 'What is your name?',
 				'Validation': function (input) {
 					return input.trim ().length > 0;
 				},
@@ -26,7 +110,7 @@ monogatari.script ({
 				'Warning': 'You must enter a name!'
 			}
 		},
-		'nvl hello {{player.name}}',
+		'y Hi {{player.name}} Welcome to Monogatari!',
 		{
 			'Choice': {
 				'Dialog': 'y Have you already read some documentation?',
@@ -57,7 +141,6 @@ monogatari.script ({
 
 		'y Go ahead and create an amazing Game!',
 		'y I can’t wait to see what story you’ll tell!',
-		//'stop music themeMusic',
 		'end'
 	]
 });
